c8da5cbf4bf9c90793df5068991064f3c52d9538
notAnotherAverageJoe <joeskokan20@.com>
2024-04-02 20:13:14 -0400

    completed

--- flask-survey/app.py
+++ flask-survey/app.py
@@ -1,12 +1,13 @@
-from flask import Flask, request, render_template, redirect, flash, session
+from flask import Flask, session, request, render_template, redirect, make_response, flash
 from flask_debugtoolbar import DebugToolbarExtension
-from surveys import satisfaction_survey as survey
+from surveys import surveys
 
 # key names will use to store some things in the session;
 # put here as constants so we're guaranteed to be consistent in
 # our spelling of these
+
 CURRENT_SURVEY_KEY = 'current_survey'
-RESPONSES_KEY = "responses"
+RESPONSES_KEY = 'responses'
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "never-tell!"
@@ -14,11 +15,30 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
+
 @app.route("/")
-def show_survey_start():
+def show_pick_survey_form():
+    """Show pick-a-survey form."""
+
+    return render_template("pick-survey.html", surveys=surveys)
+
+
+@app.route("/", methods=["POST"])
+def pick_survey():
     """Select a survey."""
 
-    return render_template("survey_start.html", survey=survey)
+    survey_id = request.form['survey_code']
+
+    # don't let them re-take a survey until cookie times out
+    if request.cookies.get(f"completed_{survey_id}"):
+        return render_template("already-done.html")
+
+    survey = surveys[survey_id]
+    session[CURRENT_SURVEY_KEY] = survey_id
+
+    return render_template("survey_start.html",
+                           survey=survey)
+
 
 @app.route("/begin", methods=["POST"])
 def start_survey():
@@ -33,16 +53,20 @@ def start_survey():
 def handle_question():
     """Save response and redirect to next question."""
 
-    # get the response choice
     choice = request.form['answer']
+    text = request.form.get("text", "")
+
+    # add this response to the list in the session
+    responses = session[RESPONSES_KEY]
+    responses.append({"choice": choice, "text": text})
 
     # add this response to the session
-    responses = session[RESPONSES_KEY]
-    responses.append(choice)
     session[RESPONSES_KEY] = responses
+    survey_code = session[CURRENT_SURVEY_KEY]
+    survey = surveys[survey_code]
 
     if (len(responses) == len(survey.questions)):
-        
+        # They've answered all the questions! Thank them.
         return redirect("/complete")
 
     else:
@@ -52,14 +76,17 @@ def handle_question():
 @app.route("/questions/<int:qid>")
 def show_question(qid):
     """Display current question."""
+
     responses = session.get(RESPONSES_KEY)
+    survey_code = session[CURRENT_SURVEY_KEY]
+    survey = surveys[survey_code]
 
     if (responses is None):
         # trying to access question page too soon
         return redirect("/")
 
     if (len(responses) == len(survey.questions)):
-        
+        # They've answered all the questions! Thank them.
         return redirect("/complete")
 
     if (len(responses) != qid):
@@ -68,19 +95,45 @@ def show_question(qid):
         return redirect(f"/questions/{len(responses)}")
 
     question = survey.questions[qid]
+
     return render_template(
         "question.html", question_num=qid, question=question)
 
 
 @app.route("/complete")
-def complete():
-    """Survey complete. Show completion page."""
+def say_thanks():
+    """Thank user and list responses."""
 
-    return render_template("completion.html")
+    survey_id = session[CURRENT_SURVEY_KEY]
+    survey = surveys[survey_id]
+    responses = session[RESPONSES_KEY]
+
+    html = render_template("completion.html",
+                           survey=survey,
+                           responses=responses)
+
+    # Set cookie noting this survey is done so they can't re-do it
+    response = make_response(html)
+    response.set_cookie(f"completed_{survey_id}", "yes", max_age=60)
+    return response
 
 
-@app.route("/responses")
-def show_responses():
-    """Display the responses collected during the survey."""
-    responses = session.get(RESPONSES_KEY, [])
-    return render_template("responses.html", responses=responses)
+
+#A test to see the test info
+
+#from flask import Flask, session, request
+#import logging
+
+#app = Flask(__name__)
+#app.config['SECRET_KEY'] = "never-tell!"
+
+# Configure logging to output to the terminal
+#logging.basicConfig(level=logging.DEBUG)
+
+#@app.route('/')
+#def index():
+    #app.logger.debug('Session data: %s', session)
+    #return 'Hello, World!'
+
+#if __name__ == '__main__':
+    #app.run(debug=True)
